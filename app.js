043c762d7b89837d7855d3e5f782a415c4efabc6
Rachit Patel <rachitp509@gmail.com>
2022-09-03 15:03:10 +0530

    changed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import Error from "./middlewares/Error.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 config({
   path: "./config/config.env",
@@ -13,6 +14,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 // import routes
 import course from "./routes/courseRoutes.js";
@@ -26,5 +34,11 @@ app.use("/api/v1", payment);
 app.use("/api/v1", other);
 
 export default app;
+
+app.get("/", (req, res) =>
+  res.send(
+    `<h1>Its working. Click to visit <a href=${process.env.FRONTEND_URL}></a></h1>`
+  )
+);
 
 app.use(Error);
